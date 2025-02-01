cf84881f4e614b63bae5ebec78188e8191489a96
may h <mhoque95@gmail.com>
2025-02-01 18:28:30 -0500

    Add NeoGit

--- init.lua
+++ init.lua
@@ -230,6 +230,20 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>g', require('neogit').open, { desc = 'Open NeoGit' })
+    end,
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
