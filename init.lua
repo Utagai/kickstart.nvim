01e956e2a0f81ed182889e96de7361ff3a30db6d
may h <mhoque95@gmail.com>
2025-02-01 18:30:25 -0500

    Add return to prior buffer

--- init.lua
+++ init.lua
@@ -163,6 +163,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Go back to prior buffer.
+vim.keymap.set('n', '<leader>bb', ':b#<CR>', { desc = 'Switch to the previous buffer' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
