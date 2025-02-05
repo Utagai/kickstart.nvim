7a990c8aef306a08b0c5ce24890e4aa082f38bea
may h <may.hoque@mongodb.com>
2025-02-05 20:18:47 +0000

    Add proto language server

--- init.lua
+++ init.lua
@@ -556,6 +556,7 @@ require('lazy').setup({
         pylsp = {},
         jsonls = {},
         gopls = {},
+        buf_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
