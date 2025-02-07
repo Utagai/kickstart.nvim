15d9839bc854890c6b404389518bdf13ffbfccdc
may h <may.hoque@mongodb.com>
2025-02-07 18:10:26 +0000

    Add back gitsigns mostly for blame functionality

--- init.lua
+++ init.lua
@@ -217,6 +217,19 @@ require('lazy').setup({
     end,
   },
 
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
